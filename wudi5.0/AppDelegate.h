f76133701d793b9984e3233e2b15b62a36528271
无敌帅枫edc <404981071@qq.com>
2018-05-28 18:07:44 +0800

    Initial Commit

---
+++ wudi5.0/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  wudi5.0
+//
+//  Created by 无敌帅枫 on 2018/5/28.
+//  Copyright © 2018年 无敌帅枫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ wudi5.0/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wudi5.0
+//
+//  Created by 无敌帅枫 on 2018/5/28.
+//  Copyright © 2018年 无敌帅枫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
